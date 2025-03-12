3e51e1a58b34845b87c266b0779ec9ab85eb0737
sbruckner <sbruckner@users.noreply.github.com>
2025-03-12 15:40:44 +0000

    Deploying to gh-pages from @ sbruckner/sbruckner.github.io@25068f07a85a36f56a63b405330c900cd8190e35 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -71,13 +71,6 @@ ninja.data = [{
           window.open("https://orcid.org/0000-0002-0885-8402", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
